a5c711b0610fdeb09b7ceaa040817bd5d9d2c79c
didiercauvin <didier.cauvin@gmail.com>
2014-10-26 17:30:32 +0100

    fix path to assets

--- js/PreloaderState.js
+++ js/PreloaderState.js
@@ -7,13 +7,13 @@ var preloaderState = (function() {
 				_preloadText = game.add.text(80, 150, 'Loading assets, please wait...', {font:"20px Desyrel", fill:"#ffffff"});
 			},
 			preload : function() {
-				game.load.image('background', '../assets/background.png');
-				game.load.image('block', '../assets/block.png');
-			    game.load.text('level1', '../levels/level1.txt');
-			    game.load.text('level2', '../levels/level2.txt');
-			    game.load.text('level3', '../levels/level3.txt');
-			    game.load.image('squary', '../assets/squary.png');
-			    game.load.image('bonus', '../assets/bonus.png');
+				game.load.image('background', 'assets/background.png');
+				game.load.image('block', 'assets/block.png');
+			    game.load.text('level1', 'levels/level1.txt');
+			    game.load.text('level2', 'levels/level2.txt');
+			    game.load.text('level3', 'levels/level3.txt');
+			    game.load.image('squary', 'assets/squary.png');
+			    game.load.image('bonus', 'assets/bonus.png');
 			},
 			create : function() {				
 				// _preloadText.text = 'Assets loaded, press space bar...';
